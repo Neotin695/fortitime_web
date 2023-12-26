3e84ad19bf4e8442789e1c76b43ed0e12b04c7dd
Neotin695 <mehani695@gmail.com>
2023-12-26 09:41:28 -0800

    fix image

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "api.uzmart.org",
+        hostname: "api.fortitime.com",
         port: "",
         pathname: "/storage/**",
       },
